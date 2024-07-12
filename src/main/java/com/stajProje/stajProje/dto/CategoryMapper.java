5886e5af227721b32e112777cb85ac7bb5cdfcb6
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-07-12 10:39:43 +0300

    something chanced

---
+++ src/main/java/com/stajProje/stajProje/dto/CategoryMapper.java
@@ -0,0 +1,4 @@
+package com.stajProje.stajProje.dto;
+
+public interface CategoryMapper {
+}
--- src/main/java/com/stajProje/stajProje/security/BasicAuthenticationSecurityConfig.java
+++ src/main/java/com/stajProje/stajProje/security/BasicAuthenticationSecurityConfig.java
@@ -1,12 +1,10 @@
-package com.stajProje.stajProje;
+package com.stajProje.stajProje.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.security.Security;
 
 @Configuration
 public class BasicAuthenticationSecurityConfig  {
